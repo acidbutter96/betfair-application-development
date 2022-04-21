d95994b6b3d452de4895f93d7ededc0ddc404b52
Marcos Pereira <marcos.p.10@hotmail.com>
2022-04-21 16:34:01 -0300

    build: create rest request method at BettingAPI object

--- app.py
+++ app.py
@@ -2,6 +2,20 @@ from services.api import BettingAPI
 
 betfair = BettingAPI('marcosp199610', 'Mmm.415263', 'IJE2hh59JFLsqo1Z')
 data = {"filter": {}}
-print(data)
 
-teste = betfair.json_rpc_req('listEventTypes', data)
+#event types list
+event_types_list = betfair.json_rpc_req('listEventTypes', data)
+event_list = betfair.rest_req('listEvents',
+                              {"filter": {
+                                  "eventIds": ["31396311"]
+                              }})
+
+# event_list
+competitions_list = betfair.json_rpc_req(
+    'listCompetitions', {"filter": {
+        "textQuery": ["31392911"]
+    }})
+""" f = open("competition_list.json", "x")
+f.write(competitions_list[0]) """
+# competition
+#event_types_list = betfair.json_rpc_req('listEventTypes', data)
--- services/api.py
+++ services/api.py
@@ -36,14 +36,30 @@ class BetFairAPI:
 
 class BettingAPI(BetFairAPI):
     __s = requests.Session()
-    url = 'https://api.betfair.com/exchange/betting/json-rpc/v1'
+    json_rpc_url = 'https://api.betfair.com/exchange/betting/json-rpc/v1'
+    REST_url = 'https://api.betfair.com/exchange/betting/rest/v1.0/'
+
+    @staticmethod
+    def __res_parser(exception, response, error_key, rest=False):
+        if exception != '':
+            raise Exception(exception)
+            return
+        if error_key or response.status_code != 200:
+            raise Exception(
+                "Bad request\n response code: {}\n Body response: {}".format(
+                    response.status_code, response.content, exception))
+        if not rest:
+            return response.json()['result'], response.json(
+            ), response.status_code, response.url
+        return response.json(), response.json(
+        ), response.status_code, response.url
 
     def __init__(self, name, password, x_application_id):
         self.__s.cert = ('./certs/client-2048.crt', './certs/client-2048.pem')
         super().__init__(name, password, x_application_id)
 
-    def __json_rpc_req(self, operation_name: str, data) -> tuple:
-        request_url = self.url  #+ '{}'.format(operation_name)
+    def json_rpc_req(self, operation_name: str, data) -> tuple:
+        request_url = self.json_rpc_url  #+ '{}'.format(operation_name)
         headers = {
             'X-Application': self.x_application_id,
             'Accept': 'application/json',
@@ -57,16 +73,32 @@ class BettingAPI(BetFairAPI):
             "id": 1
         }
         request_body = json.dumps(rb)
+        exception = ''
         try:
             response = self.__s.post(url=request_url,
                                      data=request_body,
                                      headers=headers)
         except Exception as e:
-            response
+            exception = str(e)
         error_key = list(response.json().keys()).count('error') == 1
-        if error_key or response.status_code != 200:
-            raise Exception(
-                "Bad request\n response code: {}\n Body response: {}".format(
-                    response.status_code, response.content))
-        return response.json()['result'], response.json(
-        ), response.status_code, response.url
+        return self.__res_parser(exception, response, error_key)
+
+    def rest_req(self, operation_name: str, data: dict) -> tuple:
+        request_url = '{}{}/'.format(
+            self.REST_url, operation_name)  #+ '{}'.format(operation_name)
+        headers = {
+            'X-Application': self.x_application_id,
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'X-Authentication': self.session_token
+        }
+        rb = data
+        request_body = json.dumps(rb)
+        exception = ''
+        try:
+            response = self.__s.post(url=request_url,
+                                     data=request_body,
+                                     headers=headers)
+        except Exception as e:
+            exception = str(e)
+        return self.__res_parser(exception, response, False, True)
