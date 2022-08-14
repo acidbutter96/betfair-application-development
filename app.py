effbaffe11381b37f23f113f8100d7561ea3b71e
Marcos Pereira <marknotas@outlook.com>
2022-08-14 00:24:51 -0300

    refactor: apply SOLID concepts

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from utils.dotenv import NAME, PASSWORD, X_APPLICATION_ID
 
 betfair = DataFrameParser(NAME, PASSWORD, X_APPLICATION_ID)
 betfair.first_cycle()
-asyncio.run(betfair.second_cycle())
-betfair.third_cycle()
+# asyncio.run(betfair.second_cycle())
+# betfair.third_cycle()
 # event types list
 # competition
 #event_types_list = betfair.json_rpc_req('listEventTypes', data)
