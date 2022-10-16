972180910b3b28b50230878c782f712531a891a0
Marcos Pereira <marknotas@outlook.com>
2022-10-16 13:36:29 -0300

    feature: add logger to the project

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 import asyncio
 
+from config import Logger, config
+from resources.bet import Bet
 from resources.data_treatment import DataFrameParser
-from utils.dotenv import NAME, PASSWORD, X_APPLICATION_ID
 
-betfair = DataFrameParser(NAME, PASSWORD, X_APPLICATION_ID)
-betfair.first_cycle()
-asyncio.run(betfair.second_cycle())
-betfair.third_cycle()
+
+class BetfairApplication:
+    def __init__(self,):
+        self.data_frame_parser = DataFrameParser(
+            config.NAME, config.PASSWORD, config.X_APPLICATION_ID
+        )
+        self.logger = Logger().get_logger("Betfair application")
+
+    def load_from_api(self,):
+        self.data_frame_parser.first_cycle()
+        asyncio.run(self.data_frame_parser.second_cycle())
+        self.data_frame_parser.third_cycle()
+        self.df = self.data_frame_parser.df
+
+    def load_from_csv(self,):
+        self.logger.error("Testing logger")
+
 # event types list
 # competition
 #event_types_list = betfair.json_rpc_req('listEventTypes', data)
